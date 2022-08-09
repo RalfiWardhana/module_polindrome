18da93e108a76720497287954e393dfcc2d652bb
Ralfi <ralfigeophysics@gmail.com>
2022-08-09 14:26:24 +0700

    menambah function konsonan or vocal

--- polindrome.go
+++ polindrome.go
@@ -17,3 +17,32 @@ func Polindrome(kalimat string) bool {
 	}
 	return true
 }
+
+func KonsonanOrVocal(huruf string) string {
+
+	convertLowerCase := strings.ToLower(huruf)
+
+	var result string
+
+	if len(convertLowerCase) > 1 {
+		result = "Hanya bisa mtestingskan 1 huruf bukan kata atau kalimat"
+		return result
+	}
+
+	switch convertLowerCase {
+	case "a":
+		result = "Vocal"
+	case "i":
+		result = "Vocal"
+	case "u":
+		result = "Vocal"
+	case "e":
+		result = "Vocal"
+	case "o":
+		result = "Vocal"
+	default:
+		result = "Konsonan"
+	}
+
+	return result
+}
